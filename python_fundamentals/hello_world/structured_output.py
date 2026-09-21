e2339b3bdc7983672c05790512ba8e0b18091577
Zac Downie <14842@holbertonstudents.com>
2026-09-21 11:02:59 +1000

    fix: exact output

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 print("Language:", "Python")
-print("Version:", "3")
-print("Pi approx:", "3.14")
-print("Computation valid:", "True")
+print("Version:", 3)
+print("Pi approx:", 3.14)
+print("Computation valid:", 1 == 1)
