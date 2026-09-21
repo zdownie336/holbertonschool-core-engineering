d35fb6ef22fcce860d59af6f8fd16d879ffcd15f
Zac Downie <14842@holbertonstudents.com>
2026-09-21 12:43:21 +1000

    change: range is -100 to 100

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-number = __import__("random").randint(-10, 10)
+number = __import__("random").randint(-100, 100)
 
 if number < 0:
     print(number, "is negative")
 elif number == 0:
     print(number, "is zero")
-else:
+elif number > 0:
     print(number, "is positive")
