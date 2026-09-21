2670bbd37517fd5accb0c7f3a880fffdc3ae32ba
Zac Downie <14842@holbertonstudents.com>
2026-09-21 17:18:49 +1000

    fix: white spaces and ordering

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -2,9 +2,9 @@
 
 number = __import__("random").randint(-100, 100)
 
-if number < 0:
-    print(number, "is negative")
-elif number > 0:
+if number > 0:
     print(number, "is positive")
+elif number < 0:
+    print(number, "is negative")
 else:
     print(number, "is zero")
