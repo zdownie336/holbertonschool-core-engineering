152f5664559ffcfaa5430b135706585b8195ce88
Zac Downie <14842@holbertonstudents.com>
2026-09-22 08:33:51 +1000

    fix: remove newline

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,8 +4,8 @@ last_digit = abs(number) % 10
 
 print("Last digit of ", number, "is", last_digit, "and ", end='')
 if number > 5:
-    print("is greater than 5")
+    print("is greater than 5", end='')
 elif number < 5 & number != 0:
-    print("is less than 6 and not 0")
+    print("is less than 6 and not 0", end='')
 elif number == 0:
-    print("is zero")
+    print("is zero", end='')
