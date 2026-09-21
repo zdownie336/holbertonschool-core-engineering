5cde8e3c631fe1669f4fa8850289c96ce680e633
Zac Downie <14842@holbertonstudents.com>
2026-09-21 17:42:53 +1000

    Feat: add last_digit.py

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+number = __import__("random").randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+print("Last digit of ", number, "is", last_digit, "and ", end="")
+if number > 5:
+    print("is greater than 5")
+elif number < 5 & number != 0:
+    print("is less than 6 and not 0")
+elif number == 0:
+    print("is zero")
