7ee182057290a8ed0378486eb0422aca87dbea63
Zac Downie <14842@holbertonstudents.com>
2026-09-22 09:03:25 +1000

    fix: modulo and negative conversion

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
+last_digit = abs(number) % 10
 if number < 0:
-    last_digit = abs(number) % -10
-else:
-    last_digit = abs(number) % 10
+    last_digit = last_digit * -1
 
 print("Last digit of", number, "is", last_digit, "and", end=' ')
 if number > 5:
