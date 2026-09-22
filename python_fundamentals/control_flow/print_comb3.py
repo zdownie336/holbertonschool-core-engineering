f3c121e6289733905d3b437dd0a38bcd096caf24
Zac Downie <14842@holbertonstudents.com>
2026-09-22 10:20:39 +1000

    fix: swapped i and j value in if statement

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if j == 8 and i == 9:
+        if i == 8 and j == 9:
             print("{ten}{one}".format(ten=i, one=j))
         else:
-            print("{ten}{one}, ".format(ten=i, one=j), end='')
+            print("{ten}{one}, ".format(ten=i, one=j), end="")
