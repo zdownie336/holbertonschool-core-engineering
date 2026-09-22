ad1d968099f699466c9d0dfba79a27ba8299fb55
Zac Downie <14842@holbertonstudents.com>
2026-09-22 13:32:21 +1000

    fix: changed shebang & removed print statement

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,9 +1,8 @@
-#!/usr.bin/env python3
+#!/usr/bin/env python3
 def pow(a, b):
     result = 1
     for i in range(abs(b)):
         result *= a
     if b < 0:
         result = 1 / result
-    print(a**b)
     return result
