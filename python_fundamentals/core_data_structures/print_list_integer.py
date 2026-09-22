395516d69bab5dab0e5c61c8beed56235e7a307d
Zac Downie <14842@holbertonstudents.com>
2026-09-23 09:00:33 +1000

    fix: changed for loop to num in my_list

--- python_fundamentals/core_data_structures/print_list_integer.py
+++ python_fundamentals/core_data_structures/print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{num:d}".format(num=my_list[i]))
+    for num in my_list:
+        print("{:d}".format(num))
