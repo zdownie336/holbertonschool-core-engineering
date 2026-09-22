62d95fb421ec18806021451956c44f126f3f961b
Zac Downie <14842@holbertonstudents.com>
2026-09-22 15:15:27 +1000

    fix: changed x to *

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     b = 5
     print("{n1} + {n2} = {res}".format(n1=a, n2=b, res=add(a, b)))
     print("{n1} - {n2} = {res}".format(n1=a, n2=b, res=sub(a, b)))
-    print("{n1} x {n2} = {res}".format(n1=a, n2=b, res=mul(a, b)))
+    print("{n1} * {n2} = {res}".format(n1=a, n2=b, res=mul(a, b)))
     print("{n1} / {n2} = {res}".format(n1=a, n2=b, res=div(a, b)))
