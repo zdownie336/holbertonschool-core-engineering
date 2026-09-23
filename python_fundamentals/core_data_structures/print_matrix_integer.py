1ce1749147ec4cccb0acb5da7f95b8aea422f409
Zac Downie <14842@holbertonstudents.com>
2026-09-23 10:09:42 +1000

    fix: string formatting

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -3,9 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for element in range(len(matrix[row])):
             if element == len(matrix[row]) - 1:
-                print("{num:d}".format(num=matrix[row][element]))
+                print("{:d}".format(matrix[row][element]))
             else:
-                print("{num:d}".format(num=matrix[row][element]), end=" ")
+                print("{:d}".format(matrix[row][element]), end=" ")
 
     if matrix == [[]]:
         print(f"")
