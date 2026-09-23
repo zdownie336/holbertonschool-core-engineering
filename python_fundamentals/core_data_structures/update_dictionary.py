a5a5521dcabb11c29d1df3af9aac4c0a22359feb
Zac Downie <14842@holbertonstudents.com>
2026-09-23 13:28:10 +1000

    fix: simplified

--- python_fundamentals/core_data_structures/update_dictionary.py
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    elif key not in a_dictionary:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
 
     return a_dictionary
