c8ec864ba78e97fcdc050c5e4a88ae9d94074ab7
Zac Downie <14842@holbertonstudents.com>
2026-09-23 10:03:23 +1000

    feat: add print_matrix_integer.py

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for element in range(len(matrix[row])):
+            if element == len(matrix[row]) - 1:
+                print("{num:d}".format(num=matrix[row][element]))
+            else:
+                print("{num:d}".format(num=matrix[row][element]), end=" ")
